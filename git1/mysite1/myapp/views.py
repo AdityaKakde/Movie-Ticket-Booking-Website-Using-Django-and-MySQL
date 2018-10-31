e50ed5a87c20218b2ae673f34ed24235067a43b5
AbhilashBharadwaj <abhilash.sampath@yahoo.in>
2018-10-31 19:47:22 +0530

    Add files via upload

---
+++ git1/mysite1/myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'myapp/index.html')
+
+def createEvent(request):
+    return render(request,'myapp/createEvent.html')
+
+def adminLogin(request):
+    return render(request,'myapp/adminLogin.html')
+
+def adminLobby(request):
+    return render(request,'myapp/adminLobby.html')
+
+def createAccount(request):
+    return render(request,'myapp/createAccount.html')
+
+def login(request):
+    return render(request,'myapp/login.html')    
